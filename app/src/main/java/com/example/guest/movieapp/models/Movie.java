15304224e1bc9acd8b2bf567a87eef07a2b4f0cc
Chance Magno and Gloria Friesen <gloria@>
2017-06-01 10:44:46 -0700

    complete Movie model

--- app/src/main/java/com/example/guest/movieapp/models/Movie.java
+++ app/src/main/java/com/example/guest/movieapp/models/Movie.java
@@ -5,4 +5,43 @@ package com.example.guest.movieapp.models;
  */
 
 public class Movie {
+    private String mTitle;
+    private String mPhoto;
+    private String mOverview;
+    private String mReleaseDate;
+    private String mRating;
+
+    public Movie() {
+
+    }
+
+    //constructor
+    public Movie(String title, String photo, String overview, String releaseDate, String rating) {
+        this.mTitle = title;
+        this.mPhoto = photo;
+        this.mOverview = overview;
+        this.mReleaseDate = releaseDate;
+        this.mRating = rating;
+    }
+
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getPhoto() {
+        return mPhoto;
+    }
+
+    public String getOverview() {
+        return mOverview;
+    }
+
+    public String getReleaseDate() {
+        return mReleaseDate;
+    }
+
+    public String getRating() {
+        return mRating;
+    }
 }
