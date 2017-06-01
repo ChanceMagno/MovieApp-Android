44ddcdd4abf6b0d60aa97b92ba2608edb8b286bb
Chance Magno and Gloria Friesen <gloria@>
2017-06-01 11:32:17 -0700

    add url builder, started jsonData request handling

--- app/src/main/java/com/example/guest/movieapp/services/MovieService.java
+++ app/src/main/java/com/example/guest/movieapp/services/MovieService.java
@@ -1,8 +1,47 @@
 package com.example.guest.movieapp.services;
 
+import android.app.DownloadManager;
+import android.util.Log;
+
+import com.example.guest.movieapp.Constants;
+import com.example.guest.movieapp.models.Movie;
+
+import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * Created by Guest on 6/1/17.
  */
 
 public class MovieService {
+
+    public static void searchTitles(String title, Callback callback) {
+        OkHttpClient client = new OkHttpClient.Builder().build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.APP_ID, Constants.API_KEY);
+        urlBuilder.addQueryParameter(Constants.QUERY_PARAMETER, title);
+
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder().url(url).build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+
+    public ArrayList<Movie> processResults(Response response) {
+        ArrayList<Movie> movies = new ArrayList<>();
+
+        try{
+            String jsonData = response.body().string();
+        }
+    }
+
 }
--- app/src/main/java/com/example/guest/movieapp/ui/MovieActivity.java
+++ app/src/main/java/com/example/guest/movieapp/ui/MovieActivity.java
@@ -1,9 +1,17 @@
 package com.example.guest.movieapp.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.guest.movieapp.R;
+import com.example.guest.movieapp.services.MovieService;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -11,5 +19,22 @@ public class MovieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+
+        final MovieService movieService = new MovieService();
+
+        movieService.searchTitles(title, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
     }
 }
